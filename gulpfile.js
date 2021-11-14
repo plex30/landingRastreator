b2fde2c2eb630e9590db1020a9f8f805a87b62df
plex30 <elplan_be@gmail.com>
2021-11-14 21:40:44 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,102 @@
+const { src, dest, parallel, series, watch } = require('gulp');
+
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const concat = require('gulp-concat');
+const clean = require('gulp-clean');
+/* const imagemin = require('gulp-imagemin'); */
+const changed = require('gulp-changed');
+const browsersync = require('browser-sync').create();
+
+
+
+  // Clean assets
+
+
+const clear = () => {
+
+  return src('./asstes/*', {
+    read: false
+  })
+  .pipe(clean());
+}
+
+
+ // JS function
+
+ const js = () => {
+
+   const source = './src/js/*.js';
+
+   return src(source)
+   .pipe(changed(source))
+   .pipe(concat('bundle.js'))
+   .pipe(uglify())
+   .pipe(rename({
+     extname:'.min.js'
+     
+   }))
+   .pipe(dest('./assets/js/'))
+   .pipe(browsersync.stream())
+ }
+
+
+ // CSS function
+
+ const css = () => {
+
+  const source = './src/scss/*.scss';
+
+  return src(source)
+      .pipe(changed(source))
+      .pipe(sass())
+      .pipe(autoprefixer({
+          overrideBrowserslist: ['last 2 versions'],
+          cascade: false
+      }))
+      .pipe(rename({
+          extname: '.min.css'
+      }))
+      .pipe(cssnano())
+      .pipe(dest('./assets/css/'))
+      .pipe(browsersync.stream());
+}
+
+
+ // Optimize images
+
+/* const img = () => {
+
+  return src('./src/img/*')
+      .pipe(imagemin())
+      .pipe(dest('./assets/img'));
+}
+ */
+// Watch files
+
+const watchFiles = () => {
+
+  watch('./src/scss/*', css);
+  watch('./src/js/*', js);
+  /* watch('./src/img/*', img); */
+}
+
+// BrowserSync
+
+const browserSync = () => {
+
+  browsersync.init({
+      server: {
+          baseDir: './'
+      },
+      port: 3000
+  });
+}
+
+
+
+exports.watch = parallel(watchFiles, browserSync);
+exports.default = series(clear, parallel(js, css));
---
+++ src/js/app.js
@@ -0,0 +1,90 @@
+window.onload = () => {
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 10,
+    width: 156,
+    direction: "horizontal",
+    loop: false,
+    breakpoints: {
+        768: {
+          slidesPerView:5,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 40,
+        },
+      }
+  });
+
+  fecthDataSeguros();
+  fecthDataBancos();
+};
+
+const fecthDataSeguros = async () => {
+  await fetch("http://127.0.0.1:8000/seguros")
+    .then((res) => res.json())
+    .then((res) => {
+      setSliderOne(res);
+    })
+    .catch((error) => {
+      console.log("Error: ", error);
+    });
+};
+
+const fecthDataBancos = async () => {
+  await fetch("http://127.0.0.1:8000/bancos")
+    .then((res) => res.json())
+    .then((res) => {
+      setSliderTwo(res);
+    })
+    .catch((error) => {
+      console.log("Error: ", error);
+    });
+};
+
+const setSliderOne = (data) => {
+  const sliderOne = document.querySelector(
+    ".l-service__slide1 .swiper .swiper-wrapper"
+  );
+  data.forEach((item) => {
+    const sliderChild = document.createElement("div");
+    sliderChild.classList.add("swiper-slide");
+    sliderOne.appendChild(sliderChild);
+    const title = document.createElement("p");
+    title.classList.add("slider-title");
+    title.textContent = item.title;
+    sliderChild.appendChild(title);
+    const from = document.createElement("p");
+    from.classList.add("slider-from");
+    from.textContent = "Desde";
+    sliderChild.appendChild(from);
+    const price = document.createElement("p");
+    price.classList.add("slider-price");
+    price.textContent = item.price_from + " €/mes";
+    sliderChild.appendChild(price);
+  });
+};
+
+const setSliderTwo = (data) => {
+  console.log(data);
+  const sliderTwo = document.querySelector(
+    ".l-service__slide2 .swiper .swiper-wrapper"
+  );
+  data.forEach((item) => {
+    const sliderChild = document.createElement("div");
+    sliderChild.classList.add("swiper-slide");
+    sliderTwo.appendChild(sliderChild);
+    const title = document.createElement("p");
+    title.classList.add("slider-title");
+    title.textContent = "Hasta " + item.max + " €";
+    sliderChild.appendChild(title);
+    const from = document.createElement("p");
+    from.classList.add("slider-from");
+    from.textContent = "Cuota desde";
+    sliderChild.appendChild(from);
+    const price = document.createElement("p");
+    price.classList.add("slider-price");
+    price.textContent = item.fee + " €/mes";
+    sliderChild.appendChild(price);
+  });
+};
